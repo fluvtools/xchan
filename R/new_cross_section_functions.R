275bb23a4a621bdfb2d2116516dfb01dcc377030
Vincenzo Coia <vincenzo.coia@gmail.com>
2025-02-20 13:18:29 -0800

    Make sxchan loadable

--- R/new_cross_section_functions.R
+++ R/new_cross_section_functions.R
@@ -150,12 +150,12 @@ lb_height <- function(xs) {
   xs$left$bank[2] - xs$left$thalweg[2]
 }
 
-test_that("left and right bank widths add up to full width.", {
-  w <- xs_width(my_xs)
-  wl <- lb_width(my_xs)
-  wr <- rb_width(my_xs)
-  expect_equal(w, wl + wr)
-})
+# test_that("left and right bank widths add up to full width.", {
+#   w <- xs_width(my_xs)
+#   wl <- lb_width(my_xs)
+#   wr <- rb_width(my_xs)
+#   expect_equal(w, wl + wr)
+# })
 
 #' @export
 rb_height <- function(xs) {
