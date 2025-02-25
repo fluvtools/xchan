075c50ef0c7f3f119ffec83857d6f020ad4c5678
Vincenzo Coia <vincenzo.coia@gmail.com>
2025-02-25 12:28:36 -0800

    fix get_point() -> get_points()

--- R/get_eroded_area.R
+++ R/get_eroded_area.R
@@ -2,15 +2,19 @@
 #' @export
 get_eroded_area <- function(xsec1, xsec2){
 
-  xsec1_df <- data.frame(relative_distance = c(xsec1[["left"]][["multiline"]][,1],xsec1[["right"]][["multiline"]][,1] ),
-                         elevation = c(xsec1[["left"]][["multiline"]][,2],xsec1[["right"]][["multiline"]][,2] ))
+  xsec1_df <- data.frame(
+    relative_distance = c(xsec1[["left"]][["multiline"]][,1], xsec1[["right"]][["multiline"]][,1]),
+    elevation =         c(xsec1[["left"]][["multiline"]][,2], xsec1[["right"]][["multiline"]][,2])
+  )
   xsec1_df <- xsec1_df[!duplicated(xsec1_df), ]
   xsec1_df$relative_distance <- xsec1_df$relative_distance - min(xsec1_df$relative_distance)
   xsec1_df$elevation <- xsec1_df$elevation -  min(xsec1_df$elevation)
 
 
-  xsec2_df <- data.frame(relative_distance = c(xsec2[["left"]][["multiline"]][,1],xsec2[["right"]][["multiline"]][,1] ),
-                         elevation = c(xsec2[["left"]][["multiline"]][,2],xsec2[["right"]][["multiline"]][,2] ))
+  xsec2_df <- data.frame(
+    relative_distance = c(xsec2[["left"]][["multiline"]][,1], xsec2[["right"]][["multiline"]][,1]),
+    elevation =         c(xsec2[["left"]][["multiline"]][,2], xsec2[["right"]][["multiline"]][,2])
+  )
   xsec2_df <- xsec2_df[!duplicated(xsec2_df), ]
   xsec2_df$relative_distance <- xsec2_df$relative_distance - min(xsec2_df$relative_distance)
   xsec2_df$elevation <- xsec2_df$elevation - min(xsec2_df$elevation)
@@ -37,5 +41,4 @@ get_eroded_area <- function(xsec1, xsec2){
 
   ErodedArea <- abs(area2 - area1)
   return(ErodedArea)
-
 }
