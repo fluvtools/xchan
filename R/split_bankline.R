f0f573e79b3c681c78ac51367fc04fb5d2c0611d
Jane Wang <jwang@bgcengineering.ca>
2025-05-12 14:06:43 -0700

    Extend centerline for spliting banklines

--- R/split_bankline.R
+++ R/split_bankline.R
@@ -36,6 +36,45 @@ split_bankline <- function(bankline, centerline = NULL) {
   # Extract the resulting lines
   split_parts <- sf::st_collection_extract(split, "LINESTRING")
 
+  # extend the bankline if split_parts has length of 1
+  if (length(split_parts) < 2) {
+    first_point <- as.numeric(centerline[[1]][1,])
+    last_point <- as.numeric(centerline[[1]][nrow(centerline[[1]]),])
+
+    direction <- last_point - first_point
+    direction <- direction / sqrt(sum(direction^2))
+
+    # Extend the points x/2 on each side
+    len <- as.numeric(st_length(centerline)[1])
+    new_first_point <- first_point - (len/2) * direction
+    new_last_point <- last_point + (len/2) * direction
+
+    # Create the new extended line
+    extended_line <- st_sfc(st_linestring(
+      rbind(
+        new_first_point,
+        sf::st_coordinates(centerline),
+        new_last_point)
+      )
+    )
+
+    original_crs <- st_crs(centerline)
+    extended_line <- st_set_crs(extended_line, original_crs)
+
+    # Get the intersection of the bankline and centerline
+    intersection <- sf::st_intersection(bankline, extended_line)
+
+    # Convert the bankline boundary to a centerline
+    polygon_boundary <- sf::st_cast(bankline, "MULTILINESTRING")
+
+    # Split the bankline boundary by the intersection line
+    split <- lwgeom::st_split(polygon_boundary, intersection)
+
+    # Extract the resulting lines
+    split_parts <- sf::st_collection_extract(split, "LINESTRING")
+
+  }
+
   l <- vapply(split_parts, sf::st_length, FUN.VALUE = numeric(1L))
   to_combine <- l != max(l)
 
