880b961a173a97b0dc301b6617bd3a926660847b
Jane Wang <janewyx11@gmail.com>
2026-04-08 12:45:03 -0700

    Update readme

--- data-raw/fraser_dem.R
+++ data-raw/fraser_dem.R
@@ -6,4 +6,5 @@ fraser_dem <- terra::rast(here::here("data-raw", "fraser_dem.tif"))
 fraser_dem <- terra::project(fraser_dem, paste0("EPSG:", target_crs))
 # SpatRaster objects do not serialize safely in package data; wrap before saving.
 fraser_dem <- terra::wrap(fraser_dem, proxy = FALSE)
+
 usethis::use_data(fraser_dem, overwrite = TRUE)
